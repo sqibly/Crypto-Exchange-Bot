d7bbfc6c64f9d06299bcb3654c4d82732797c7d4
sqibly <ironmusktin@gmail.com>
2017-11-18 12:44:45 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import random
+
+def main():
+    hc = random.uniform(1, 5)
+    hp = 1
+    print(hc)
+
+    test = stock()
+    while True:
+        test.cost
+        test.choice
+        test.learn
+        hc = random.uniform(1, 5)
+    
+class stock():
+    def __init__(self):
+         #self.quote = quote
+        self.hp = hp
+        self.hc = hc
+        self.last_action
+       #web.DataReader(self.quote, 'yahoo', start, end)
+    def cost():
+        2(hp-hc)-1+n
+    def learn():
+        if last_action==hold:
+            self.hold_weight-=2*(hp-hc)^0.8
+        elif last_action==sell:
+            self.sell_weight-=2*(hp-hc)^0.8
+        elif last_action==buy:
+            self.buy_weight-=2*(hp-hc)^0.8
+    def choice():
+        #if cell_mult_1 = #last action is the highest mult
+        sell_weight = 1.0
+        hold_weight = 1.0
+        buy_weight = 1.0
+        
+        if sell_weight > hold_weight and sell_weight > buy_weight:
+            dollars-=price
+            asset+=1 
+        elif buy_weight > hold_weight and buy_weight > sell_weight:
+            dollars+=price
+            asset-=1
+        elif hold_weight > buy_weight and hold_weight > sell_weight:
+            pass
+
+    def recalc_price():
+        #read in new price and change hc to it.
+        pass
+##
+##  def sell():
+##      dollars+=price
+##      asset-=1    
+
+##  def buy():
+##      dollars-=price
+##      asset+=1
+##
+##  def hold():
+##      pass
+
+    def die():
+        self.close()
+    def clone():
+        self.copy()
+
+if __name__ == '__main__':
+    main()
+    
+    
